0842d6ae7b291671980224b149a03db79742f4be
ShubhamVerma-Developer <shubham25122001@gmail.com>
2025-02-08 21:06:31 +0530

    design the search in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,16 @@ const Navbar = () => {
   return (
     <div className="flex item-center justify-between p-4">
       {/* SEARCH BAR */}
-      <div className="hidden md:flex">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="" width={14} height={14} />
-        <input type="text" placeholder="Search..." />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-[200px] p-2 bg-transparent outline-none"
+        />
       </div>
       {/* ICONS AND USER */}
-      <div className="flex item-center gap-6 ">
+      <div className="flex item-center gap-6 justify-end w-full">
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/message.png" alt="" width={20} height={20} />
         </div>
