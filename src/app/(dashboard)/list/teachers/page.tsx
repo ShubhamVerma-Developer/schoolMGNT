2808f456086bd0b0f0b5108d1d4f5bb9805793ff
ShubhamVerma-Developer <shubham25122001@gmail.com>
2025-02-15 15:37:12 +0530

    create the table search and filter sort and plus button in the teacher list page

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -10,9 +10,15 @@ const TeacherListPage = () => {
         <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
-          <div className="">
-            <button>
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/filter.png" alt="" width={10} height={10} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="" width={10} height={10} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/plus.png" alt="" width={10} height={10} />
             </button>
           </div>
         </div>
