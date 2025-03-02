be6054cb6386149790a5b3a8d0e5069142ccfdca
ShubhamVerma-Developer <shubham25122001@gmail.com>
2025-03-02 09:05:20 +0530

    create the form modal for the create update and delete

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -88,9 +89,10 @@ const TeacherListPage = () => {
           </button>
         </Link>
         {role === "admin" && (
-          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-            <Image src="/delete.png" alt="" width={16} height={16} />
-          </button>
+          // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+          //   <Image src="/delete.png" alt="" width={16} height={16} />
+          // </button>
+          <FormModal table="teacher" type="delete" id={item.id} />
         )}
       </td>
     </tr>
@@ -111,9 +113,10 @@ const TeacherListPage = () => {
               <Image src="/sort.png" alt="" width={10} height={10} />
             </button>
             {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                <Image src="/plus.png" alt="" width={10} height={10} />
-              </button>
+              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              //   <Image src="/plus.png" alt="" width={10} height={10} />
+              // </button>
+              <FormModal table="teacher" type="create" />
             )}
           </div>
         </div>
